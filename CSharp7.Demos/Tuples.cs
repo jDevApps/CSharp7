ba42e3c508eaeca53777de9a80b933ea362f6c0f
José David Rodríguez <dirod2@hotmail.com>
2017-07-05 18:47:56 -0500

    Added project with example for handling exceptions. Additionally an example of tuples in C # 7.

---
+++ CSharp7.Demos/Tuples.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace CSharp7.Demos
+{
+    public class Tuples
+    {
+        #region Methods versions previous to C# 7
+
+        public void TupleTest()
+        {
+            // Tuple with two integers
+            var coords = new Tuple<int, int>(100, 200);
+            // Creation using factory with type inference
+            var coords2 = Tuple.Create(100, 200);
+            // With three elements
+            var coords3d = Tuple.Create(100, 200, 300);
+            // With four elements of different type
+            var person = Tuple.Create("John", new DateTime(1997, 2, 3), 1.68m, true);
+
+            Console.WriteLine(coords.Item2); // 200
+            Console.WriteLine(coords3d.Item3); // 300
+            Console.WriteLine(person.Item4); // True
+        }
+
+        #endregion Methods versions previous to C# 7
+
+        #region Methods version C# 7
+
+        /// <summary>
+        /// Examples for constructing tuples and using them as parameters in functions
+        /// </summary>
+        public void TupleTest_1()
+        {
+            var coords3d = ValueTuple.Create(100, 200, 300);
+            Console.WriteLine(coords3d.Item1); // 100
+
+            // Creating tuples - form 1
+            var person = ("John", 34);
+            Console.WriteLine(person.Item1);  // John
+            Console.WriteLine(person.Item2);  // 34
+
+            // Form 2
+            var person2 = (name: "John", age: 34);
+            Console.WriteLine(person2.name); // John
+            Console.WriteLine(person2.age);  // 34
+            Console.WriteLine(person2.GetType()); // System.ValueTuple`2[System.String,System.Int32]
+
+            show(person2);
+
+            void show((string name, int age) prs)
+            {
+                Console.WriteLine($"Name: {prs.name}");
+                Console.WriteLine($"Age: {prs.age}");
+            }
+        }
+
+        /// <summary>
+        /// To consume a method that returns a tuple with named members
+        /// </summary>
+        private void TupleTest_2()
+        {
+            var person = create();
+            Console.WriteLine(person.age); // 23
+
+            (string name, int age) create() =>
+                 (name: "Test", age: 23);
+        }
+
+        /// <summary>
+        /// Deconstruction of tuples
+        /// </summary>
+        private void TupleTest_3()
+        {
+            // Form 1
+            (var name, var age, var height) = create();
+            // Form 2
+            var (name1, age1, height1) = create();
+
+            Console.WriteLine($"{name} is {age} years old");
+
+            (string name, int age, decimal height) create() => (name: "John", age: 23, height: 1.68m);
+        }
+
+        #endregion Methods version C# 7
+    }
+}
---
+++ CSharp7.ExceptionHandling/Program.cs
@@ -0,0 +1,167 @@
+﻿using Polly;
+using System;
+
+namespace CSharp7.ExceptionHandling
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            #region Reintentar
+
+            Console.WriteLine("== Reintentar ==");
+            var politicaReintenta = Policy
+                .Handle<Exception>()
+                .Retry(ReportaError);
+
+            try
+            {
+                politicaReintenta.Execute(LanzaExcepcion);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Después de los intentos, sigue fallando ({e.Message})");
+            }
+
+            var politicaReintenta5 = Policy
+                .Handle<DivideByZeroException>()
+                .Retry(5, ReportaError);
+
+            Console.WriteLine("\n== Reintentar 5 veces ==");
+
+            try
+            {
+                politicaReintenta5.Execute(LanzaExcepcion);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Después de los intentos, sigue fallando ({e.Message})");
+            }
+
+            var handleSeveralExceptions = Policy
+                .Handle<ArgumentException>()
+                .Or<DivideByZeroException>()
+                .Retry(5, ReportaError);
+
+            Console.WriteLine("\n== Reintentar 5 veces y maneja varias excepciones ==");
+
+            try
+            {
+                handleSeveralExceptions.Execute(LanzaExcepcion);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Después de los intentos, sigue fallando ({e.Message})");
+            }
+
+            #endregion Reintentar
+
+            #region Reintentar esperando tiempo
+
+            var politicaWaitAndRetry = Policy
+                .Handle<DivideByZeroException>()
+                .WaitAndRetry(new[]
+                      {
+                        TimeSpan.FromSeconds(1),
+                        TimeSpan.FromSeconds(2),
+                        TimeSpan.FromSeconds(3)
+                    }, ReportaError);
+
+            Console.WriteLine("\n== Reintentar pero esperar segundos ==");
+
+            try
+            {
+                politicaWaitAndRetry.Execute(LanzaExcepcion);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Después de los intentos, sigue fallando ({e.Message})");
+            }
+
+            var politicaWaitAndRetryExponential = Policy
+                .Handle<DivideByZeroException>()
+                .WaitAndRetry(5,
+                              intento => TimeSpan.FromSeconds(Math.Pow(2, intento)),
+                              ReportaError);
+
+            Console.WriteLine("\n== Reintentar 5 veces esperando un tiempo exponencial ==");
+
+            try
+            {
+                politicaWaitAndRetryExponential.Execute(LanzaExcepcion);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Después de los intentos, sigue fallando ({e.Message})");
+            }
+
+            #endregion Reintentar esperando tiempo
+
+            #region Reintentar esperando tiempo
+
+            var politicaWaitAndRetryString = Policy<string>
+                .Handle<Exception>()
+                .WaitAndRetry(5,
+                              intento => TimeSpan.FromSeconds(Math.Pow(2, intento)),
+                              ReportaError);
+
+            Console.WriteLine("\n== Reintentar con valor de retorno ==");
+
+            try
+            {
+                var resultado = politicaWaitAndRetryString.Execute(LanzaExcepcionConCadena);
+                Console.WriteLine($"Resultado {resultado}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Después de los intentos, sigue fallando ({e.Message})");
+            }
+
+            var politicaWithFallback = Policy<string>
+                .Handle<Exception>()
+                .Fallback("Valor de Fallback");
+
+            Console.WriteLine("\n== Reintentar esperando un valor de fallback ==");
+
+            var resultado2 = politicaWithFallback.Execute(LanzaExcepcionConCadena);
+            Console.WriteLine($"Resultado {resultado2}");
+
+            #endregion Reintentar esperando tiempo
+
+            var mixedPolicy = Policy.Wrap(politicaWaitAndRetryString, politicaWithFallback);
+
+            Console.WriteLine("\n== Polizas mezcladas ==");
+
+            var resultado3 = mixedPolicy.Execute(LanzaExcepcionConCadena);
+            Console.WriteLine($"Resultado {resultado3}");
+
+            Console.WriteLine("Terminado");
+            Console.Read();
+        }
+
+        private static void ReportaError(DelegateResult<string> resultado, TimeSpan tiempo, int intento, Context context)
+        {
+            Console.WriteLine($"Intento: {intento:00} (próximo intento en: {tiempo.Seconds} segundos)\tTiempo: {DateTime.Now}");
+        }
+
+        private static void ReportaError(Exception e, TimeSpan tiempo, int intento, Context contexto)
+        {
+            Console.WriteLine($"Intento: {intento:00} (próximo intento en: {tiempo.Seconds} segundos)\tTiempo: {DateTime.Now}");
+        }
+
+        private static void ReportaError(Exception e, int intentos)
+        {
+            Console.WriteLine($"Intento: {intentos:00}\tTiempo: {DateTime.Now}");
+        }
+
+        private static void LanzaExcepcion()
+        {
+            throw new DivideByZeroException();
+        }
+
+        private static string LanzaExcepcionConCadena()
+        {
+            throw new Exception();
+        }
+    }
+}
