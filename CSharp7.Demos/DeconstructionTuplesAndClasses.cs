7aa10b017b38c445afdd1eeb68191f30eeda69fc
José David Rodríguez <dirod2@hotmail.com>
2017-07-06 13:33:26 -0500

    Examples are added for Expression Throw and Deconstruction of tuples and classes.

---
+++ CSharp7.Demos/DeconstructionTuplesAndClasses.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp7.Demos
+{
+    public class DeconstructionTuplesAndClasses
+    {
+        #region Methods version C# 7
+
+        public void Test()
+        {
+            var sum = Sum(1, 3);
+            Console.WriteLine($"Sum: {sum}");
+
+            int Sum(int a, int b)
+            {
+                return a + b;
+            }
+        }
+
+        /// <summary>
+        /// The var keyword can be used to indicate to the compiler that it is the one that is responsible for inferring the types of the elements of the tuple
+        /// </summary>
+        public void Test_2()
+        {
+            var (add, sub, mul, div) = Calculate(1, 3);
+            Console.WriteLine($"Sum: {add}");
+
+            (int add, int sub, int mul, int div) Calculate(int a, int b)
+            {
+                return (a + b, a - b, a * b, a / b);
+            }
+        }
+
+        /// <summary>
+        /// Defining the type of each variable separately
+        /// </summary>
+        public void Test_3()
+        {
+            (int add, int sub, int mul, int div) = Calculate(1, 3);
+            (var add, var sub, var mul, var div) = Calculate(1, 3);
+            var (add, sub, mul, div) = Calculate(1, 3);
+        }
+
+        /// <summary>
+        /// You can deconstruct on previously existing variables
+        /// </summary>
+        public void Test_4()
+        {
+            int add, sub, mul, div;
+            (add, sub, mul, div) = Calculate(1, 3);
+            Console.WriteLine($"Sum: {add}");
+        }
+
+        #endregion Methods version C# 7
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+
+        public void Deconstruct(out string name, out int age, out string email)
+        {
+            name = Name;
+            age = Age;
+            email = Email;
+        }
+    }
+}
---
+++ CSharp7.Demos/ThrowExpressions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp7.Demos
+{
+    public class ThrowExpressions
+    {
+        #region Methos versions previous to C# 7
+
+        private readonly object _first;
+        private readonly object _second;
+        private readonly object _other;
+
+        public ThrowExpressions(object first, object second)
+        {
+            if (first == null)
+                throw new ArgumentNullException("first");
+
+            if (second == null)
+                throw new ArgumentNullException("second");
+
+            _first = first;
+            _second = second;
+        }
+
+        #endregion Methos versions previous to C# 7
+
+        #region Methods version C# 7
+
+        /// <summary>
+        /// In C # 7 you can simplify the syntax because throw is considered an expression for compiling purposes. That is, with the latest version of C # we can 
+        /// simplify the previous method as follows, and compile correctly
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <param name="other"></param>
+        public ThrowExpressions(object first, object second, object other)
+        {
+            _first = first ?? throw new ArgumentNullException("first");
+            _second = second ?? throw new ArgumentNullException(nameof(second));
+            _other = other ?? throw new ArgumentNullException("other");
+        }
+
+        /// <summary>
+        /// Another example of use, although of doubtful utility and readability
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int Divide(int a, int b)
+        {
+            return a / (b != 0 ? b : throw new ArgumentOutOfRangeException(nameof(b)));
+        }
+
+        #endregion Methods version C# 7
+    }
+}
